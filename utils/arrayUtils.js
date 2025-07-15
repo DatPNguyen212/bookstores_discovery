cbb58a9fc210b351995709598b4a29da7c4a604c
Dat P Nguyen <darkexpert3@gmail.com>
2025-07-15 23:58:28 +0700

    add arrayUtils.generateArray()

--- utils/arrayUtils.js
+++ utils/arrayUtils.js
@@ -6,6 +6,40 @@ const arrayUtils = {
     }
     return array[numberUtils.generateRandNum(0, array.length - 1)]
   },
+  generateArray(option) {
+    let newArray = []
+
+    if (!option.numberItems) {
+      throw new TypeError(
+        'First object parameter MUST have numberItems property'
+      )
+    } else if (option.enum && !option.default) {
+      if (!Array.isArray(option.enum)) {
+        throw new TypeError('enum property must be an array')
+      }
+      for (let i = 0; i < option.numberItems; i++) {
+        const randItemFromEnum = this.getRandItem(option.enum)
+        newArray.push(randItemFromEnum)
+      }
+    } else if (!option.enum && option.default) {
+      if (typeof option.default !== 'number') {
+        throw new TypeError('default property must be number data type')
+      }
+      for (let i = 0; i < option.numberItems; i++) {
+        newArray.push(option.default)
+      }
+    } else if (!option.enum && !option.default) {
+      throw new TypeError(
+        'First object parameter must have enum property OR default property'
+      )
+    } else {
+      throw new TypeError(
+        'First object parameter CANNOT have both default property and enum property'
+      )
+    }
+
+    return newArray
+  },
 }
 
 export default arrayUtils
--- utils/arrayUtils.test.js
+++ utils/arrayUtils.test.js
@@ -64,3 +64,99 @@ describe('arrayUtils.getRandItem()', () => {
     expect(fn).toThrow('First parameter should be of array data type')
   })
 })
+
+describe('arrayUtils.generateArray()', () => {
+  let mathRandomSpy
+
+  beforeEach(() => {
+    mathRandomSpy = vi.spyOn(Math, 'random')
+  })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  it('when pass {numberItems: 3, enum: [1,2,3]}, it should return an array that contains 3 items', () => {
+    const option = { numberItems: 3, enum: [1, 2, 3] }
+
+    const res = arrayUtils.generateArray(option)
+
+    expect(res.length).toBe(3)
+  })
+  it('given Math.random() returns 0, when pass {numberItems: 3, enum: [1, 2, 3]}, it should return an array where each item can only be 1', () => {
+    mathRandomSpy.mockImplementation(
+      vi.fn(() => {
+        return 0
+      })
+    )
+    const option = { numberItems: 3, enum: [1, 2, 3] }
+
+    const res = arrayUtils.generateArray(option)
+
+    for (let item of res) {
+      expect(item).toBe(1)
+    }
+  })
+  it('when pass {numberItems: 3, default: 3}, it should return an array where each item can only be 3', () => {
+    const option = { numberItems: 3, default: 3 }
+
+    const res = arrayUtils.generateArray(option)
+
+    for (let item of res) {
+      expect(item).toBe(3)
+    }
+  })
+
+  it('when pass {numberItems: 3} with no default or enum, it should throw an error', () => {
+    const option = { numberItems: 3 }
+
+    const fn = () => {
+      arrayUtils.generateArray(option)
+    }
+
+    expect(fn).toThrow(
+      'First object parameter must have enum property OR default property'
+    )
+  })
+
+  it('when pass option object with numberItems, enum, default properties; it should throw an error', () => {
+    const option = { numberItems: 3, default: 3, enum: [1, 2, 3] }
+
+    const fn = () => {
+      arrayUtils.generateArray(option)
+    }
+
+    expect(fn).toThrow(
+      'First object parameter CANNOT have both default property and enum property'
+    )
+  })
+
+  it("when option object doesn't have numberItems property; it should throw an error", () => {
+    const option = { default: 3, enum: [1, 2, 3] }
+
+    const fn = () => {
+      arrayUtils.generateArray(option)
+    }
+
+    expect(fn).toThrow('First object parameter MUST have numberItems property')
+  })
+  it('if enum is not array, it should throw an error', () => {
+    const option = { numberItems: 3, enum: 3 }
+
+    const fn = () => {
+      arrayUtils.generateArray(option)
+    }
+
+    expect(fn).toThrow('enum property must be an array')
+  })
+
+  it('if default property is not number data type, it should throw an error', () => {
+    const option = { numberItems: 3, default: '3' }
+
+    const fn = () => {
+      arrayUtils.generateArray(option)
+    }
+
+    expect(fn).toThrow('default property must be number data type')
+  })
+})
