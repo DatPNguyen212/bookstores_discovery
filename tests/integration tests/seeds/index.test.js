c935b486dda2ec5ba02b1498189b7f7b0aff76b7
Dat P Nguyen <darkexpert3@gmail.com>
2025-07-30 02:24:30 +0700

    add integration tests for seedBookstore()

---
+++ tests/integration tests/seeds/index.test.js
@@ -0,0 +1,93 @@
+import {
+  vi,
+  describe,
+  it,
+  expect,
+  beforeEach,
+  afterEach,
+  afterAll,
+} from 'vitest'
+import seedBookstore from '../../../seeds/index.js'
+import setupDB from '../../../config/setupDB.js'
+import models from '../../../models/index.js'
+import testDBUtils from '../../../utils/testDBUtils/testDBUtils.js'
+import dbUtils from '../../../utils/dbUtils.js'
+import seedHelpers from '../../../seeds/seedHelpers.js'
+
+vi.mock('../../../config/setupDB.js', () => {
+  return {
+    default: {
+      connect: vi.fn(async () => {
+        return Promise.resolve()
+      }),
+      close: vi.fn(async () => {}),
+    },
+  }
+})
+vi.mock('../../../utils/dbUtils.js', () => {
+  return {
+    default: {
+      clearCollection: vi.fn(async () => {}),
+    },
+  }
+})
+
+describe('seedBookstore()', () => {
+  let testConnection
+  let Bookstore
+  let bookstoreModelClassSpy
+
+  beforeEach(async () => {
+    testConnection = await testDBUtils.connect()
+    Bookstore = testConnection.model('Bookstore', models.bookstore.schema)
+    bookstoreModelClassSpy = vi
+      .spyOn(models.bookstore, 'ModelClass', 'get')
+      .mockReturnValue(Bookstore)
+  })
+  afterEach(async () => {
+    await testDBUtils.clearDB()
+    vi.restoreAllMocks()
+  })
+
+  afterAll(async () => {
+    await testDBUtils.closeDB()
+  })
+  it('when you pass a positive number to it, it should create the that same number of documents in the DB', async () => {
+    const numberOfStores = 3
+
+    await seedBookstore(numberOfStores)
+
+    const res = await Bookstore.find({})
+
+    expect(res).toHaveLength(3)
+  })
+  it('given seedHelpers.genObjForBookstoreClass returns a predetermined obj, when pass 1, it should save a predictable document to DB', async () => {
+    const objMock = {
+      name: 'John Wick',
+      address: '3, Ba Thang Hai Street, 3 District, Ho Chi Minh City',
+      description: 'test',
+      genres: ['fantasy'],
+      images: 'test',
+      openDays: ['Monday', 'Tuesday'],
+    }
+    const genObjForBookstoreClassSpy = vi
+      .spyOn(seedHelpers, 'genObjForBookstoreClass')
+      .mockReturnValue(objMock)
+    const numberOfStores = 1
+
+    await seedBookstore(numberOfStores)
+
+    const res = await Bookstore.find({})
+    console.log(res)
+
+    expect(res[0]).toHaveProperty('name', 'John Wick')
+    expect(res[0]).toHaveProperty(
+      'address',
+      '3, Ba Thang Hai Street, 3 District, Ho Chi Minh City'
+    )
+    expect(res[0]).toHaveProperty('description', 'test')
+    expect(res[0]).toHaveProperty('genres', ['fantasy'])
+    expect(res[0]).toHaveProperty('images', 'test')
+    expect(res[0]).toHaveProperty('openDays', ['Monday', 'Tuesday'])
+  })
+})
