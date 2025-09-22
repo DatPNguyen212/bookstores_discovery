225794db93364299f9bfbd3663546910591e07a4
Dat P Nguyen <darkexpert3@gmail.com>
2025-09-23 04:08:18 +0700

    refactor validators methods to return an obj that have result and errorMsg instead of just boolean

--- public/js/utils/validators.js
+++ public/js/utils/validators.js
@@ -4,11 +4,20 @@ const validators = {
       throw new TypeError('First parameter should be of string data type')
     }
     if (name.length > 100) {
-      return false
+      return {
+        result: false,
+        errorMsg: 'Name must be less than or equal to 100 characters',
+      }
     } else if (name.length === 0) {
-      return false
+      return {
+        result: false,
+        errorMsg: 'Name field must not be empty',
+      }
     } else {
-      return true
+      return {
+        result: true,
+        errorMsg: null,
+      }
     }
   },
   isValidAddress(address) {
@@ -16,11 +25,20 @@ const validators = {
       throw new TypeError('First parameter should be of string data type')
     }
     if (address.length > 255) {
-      return false
+      return {
+        result: false,
+        errorMsg: 'Address field must not be more than 255 characters',
+      }
     } else if (address.length === 0) {
-      return false
+      return {
+        result: false,
+        errorMsg: 'Address field must not be empty',
+      }
     } else {
-      return true
+      return {
+        result: true,
+        errorMsg: null,
+      }
     }
   },
 
@@ -29,11 +47,20 @@ const validators = {
       throw new TypeError('First parameter should be of string data type')
     }
     if (description.length > 500) {
-      return false
+      return {
+        result: false,
+        errorMsg: 'Description must not be more than 500 characters',
+      }
     } else if (description.length === 0) {
-      return false
+      return {
+        result: false,
+        errorMsg: 'Description must not be empty',
+      }
     } else {
-      return true
+      return {
+        result: true,
+        errorMsg: null,
+      }
     }
   },
 }
--- tests/unit tests/public/js/utils/validators.test.js
+++ tests/unit tests/public/js/utils/validators.test.js
@@ -3,26 +3,35 @@ import validators from '../../../../../public/js/utils/validators.js'
 import randomString from 'randomstring'
 
 describe('validators.isValidStoreName()', () => {
-  it('when you pass valid bookstore name where string is not empty and is less and upto 100 characters, it should return true', () => {
+  it('when you pass valid bookstore name where string is not empty and is less and upto 100 characters, it should return correct obj', () => {
     const name = randomString.generate(100)
 
     const result = validators.isValidStoreName(name)
 
-    expect(result).toBe(true)
+    expect(result).toEqual({
+      result: true,
+      errorMsg: null,
+    })
   })
-  it('when you pass an empty string, it should return false', () => {
+  it('when you pass an empty string, it should return correct obj', () => {
     const name = ''
 
     const result = validators.isValidStoreName(name)
 
-    expect(result).toBe(false)
+    expect(result).toEqual({
+      result: false,
+      errorMsg: 'Name field must not be empty',
+    })
   })
-  it('when you pass a string that is more than 100 characters, it should return false', () => {
+  it('when you pass a string that is more than 100 characters, it should return correct obj', () => {
     const name = randomString.generate(101)
 
     const result = validators.isValidStoreName(name)
 
-    expect(result).toBe(false)
+    expect(result).toEqual({
+      result: false,
+      errorMsg: 'Name must be less than or equal to 100 characters',
+    })
   })
 
   it('when you pass a non string, it should throw correct error', () => {
@@ -37,26 +46,35 @@ describe('validators.isValidStoreName()', () => {
 })
 
 describe('validators.isValidAddress()', () => {
-  it('when you pass a valid address where it is not an empty string and is less and upto 255 characters, it should return true', () => {
+  it('when you pass a valid address where it is not an empty string and is less and upto 255 characters, it should return correct obj', () => {
     const address = randomString.generate(255)
 
     const result = validators.isValidAddress(address)
 
-    expect(result).toBe(true)
+    expect(result).toEqual({
+      result: true,
+      errorMsg: null,
+    })
   })
-  it('when you pass string that is more than 255 characters, it should return false', () => {
+  it('when you pass string that is more than 255 characters, it should return correct obj', () => {
     const address = randomString.generate(256)
 
     const result = validators.isValidAddress(address)
 
-    expect(result).toBe(false)
+    expect(result).toEqual({
+      result: false,
+      errorMsg: 'Address field must not be more than 255 characters',
+    })
   })
-  it('when you pass string that is empty, it should return false', () => {
+  it('when you pass string that is empty, it should return correct obj', () => {
     const address = ''
 
     const result = validators.isValidAddress(address)
 
-    expect(result).toBe(false)
+    expect(result).toEqual({
+      result: false,
+      errorMsg: 'Address field must not be empty',
+    })
   })
 
   it('when you pass a non string, it should throw correct error', () => {
@@ -71,27 +89,36 @@ describe('validators.isValidAddress()', () => {
 })
 
 describe('validators.isValidDescription()', () => {
-  it('when you pass a string that is more than 500 characters, it should return false', () => {
+  it('when you pass a string that is more than 500 characters, it should return correct obj', () => {
     const description = randomString.generate(501)
 
     const result = validators.isValidDescription(description)
 
-    expect(result).toBe(false)
+    expect(result).toEqual({
+      result: false,
+      errorMsg: 'Description must not be more than 500 characters',
+    })
   })
-  it('when you pass an empty string, it should return false', () => {
+  it('when you pass an empty string, it should return correct obj', () => {
     const description = ''
 
     const result = validators.isValidDescription(description)
 
-    expect(result).toBe(false)
+    expect(result).toEqual({
+      result: false,
+      errorMsg: 'Description must not be empty',
+    })
   })
 
-  it('when you pass string that is not empty and is less and upto 500 characters, it should return true', () => {
+  it('when you pass string that is not empty and is less and upto 500 characters, it should return correct obj', () => {
     const description = randomString.generate(500)
 
     const result = validators.isValidDescription(description)
 
-    expect(result).toBe(true)
+    expect(result).toEqual({
+      result: true,
+      errorMsg: null,
+    })
   })
 
   it('when you pass a non string, it should throw correct error', () => {
