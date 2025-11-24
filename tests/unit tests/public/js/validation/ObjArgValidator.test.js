0bb1b4d25a4b1e25621903f64fbeddd85765a070
Dat P Nguyen <darkexpert3@gmail.com>
2025-11-24 15:56:56 +0700

    refactor ObjArgValidator.test.js to use SchemaAdapterMock class instead of directly using CreateTextOptsSchema

--- tests/unit tests/public/js/validation/ObjArgValidator.test.js
+++ tests/unit tests/public/js/validation/ObjArgValidator.test.js
@@ -2,8 +2,24 @@ import { vi, describe, it, expect, beforeEach, afterEach } from 'vitest'
 import ObjArgValidator from '../../../../../public/js/validation/ObjArgValidator.js'
 import CreateTextOptsSchema from '../../../../../public/js/validation/CreateTextOptsSchema.js'
 import ValidatorConfigSchema from '../../../../../public/js/validation/ValidatorConfigSchema.js'
+import SchemaAdapterBase, {
+  IS_SCHEMA_ADAPTER_BASE_INSTANCE,
+} from '../../../../../public/js/abstracts/validation/SchemaAdataperBase.js'
 
 describe('ObjArgValidator', () => {
+  let schemaAdapterMock
+  class SchemaAdapterMock extends SchemaAdapterBase {
+    constructor() {
+      super()
+      this[IS_SCHEMA_ADAPTER_BASE_INSTANCE] = true
+      this.name = 'test'
+    }
+  }
+
+  beforeEach(() => {
+    schemaAdapterMock = new SchemaAdapterMock()
+  })
+
   it('when you pass a non instance of SchemaAdapterBase, it should throw an error', () => {
     const schemaAdapter = 3
 
@@ -23,8 +39,7 @@ describe('ObjArgValidator', () => {
   })
 
   it('when you pass an array that contains atleast 1 non instance of SchemaAdapterBase, it should throw an error', () => {
-    const schemaAdapter = new CreateTextOptsSchema()
-    const schemaAdapters = [1, schemaAdapter]
+    const schemaAdapters = [1, schemaAdapterMock]
 
     const fn = () => {
       new ObjArgValidator(schemaAdapters)
@@ -36,8 +51,8 @@ describe('ObjArgValidator', () => {
   })
 
   it('when you pass an array that contains atleast 1 undefined item, it should throw an error', () => {
-    const schemaAdapter = undefined
-    const schemaAdapters = [1, schemaAdapter]
+    const schemaAdapterMock = undefined
+    const schemaAdapters = [1, schemaAdapterMock]
 
     const fn = () => {
       new ObjArgValidator(schemaAdapters)
@@ -48,17 +63,15 @@ describe('ObjArgValidator', () => {
     )
   })
 
-  it('when you pass a single valid instance of SingleAdapterBase to constructor, objArgValidator.schema needs to store that argument', () => {
-    const schemaAdapter = new CreateTextOptsSchema()
+  it('when you pass a single valid instance of SingleAdapterBase to constructor, new instance should store that schemaAdapter correctly', () => {
+    const result = new ObjArgValidator(schemaAdapterMock)
 
-    const result = new ObjArgValidator(schemaAdapter)
-
-    expect(result.schema).toEqual(schemaAdapter)
+    expect(result.schema).toEqual(schemaAdapterMock)
   })
 
   it('when you pass an array of instances of SchemaAdaptersBase, objArgValidator.schemas needs to store that array', () => {
-    const schemaAdapter1 = new CreateTextOptsSchema()
-    const schemaAdapter2 = new CreateTextOptsSchema()
+    const schemaAdapter1 = new SchemaAdapterMock()
+    const schemaAdapter2 = new SchemaAdapterMock()
 
     const schemaAdapters = [schemaAdapter1, schemaAdapter2]
 
