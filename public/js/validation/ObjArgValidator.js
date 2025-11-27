4072ba6925c019bea900b910d2a54b3a74e557d5
Dat P Nguyen <darkexpert3@gmail.com>
2025-11-27 07:35:27 +0700

    change ObjArgValidator to store the .schema values of the schemaAdapters you passed to it, but their property key names are the .paramName property values of the schemaAdapters

--- public/js/validation/ObjArgValidator.js
+++ public/js/validation/ObjArgValidator.js
@@ -1,4 +1,5 @@
 import { IS_SCHEMA_ADAPTER_BASE_INSTANCE } from '../abstracts/validation/SchemaAdataperBase.js'
+const IS_OBJ_ARG_VALIDATOR_INSTANCE = Symbol('ObjArgValidator/is-instance')
 
 class ObjArgValidator {
   constructor(schemaAdapters) {
@@ -17,11 +18,29 @@ class ObjArgValidator {
     }
 
     if (!Array.isArray(schemaAdapters)) {
-      this.schema = schemaAdapters
+      this[schemaAdapters.paramName] = schemaAdapters.schema
     } else {
-      this.schemas = schemaAdapters
+      for (let schemaAdapter of schemaAdapters) {
+        const paramName = schemaAdapter.paramName
+        const schema = schemaAdapter.schema
+
+        const objArgValidatorKeys = Object.keys(this)
+
+        for (let key of objArgValidatorKeys) {
+          if (key === paramName) {
+            throw new TypeError(
+              'There are SchemaAdapterBase instances with the same .paramName value. Each SchemaAdapterBase instance in the array needs to have unique .paramName value'
+            )
+          }
+        }
+
+        this[paramName] = schema
+      }
     }
+
+    this[IS_OBJ_ARG_VALIDATOR_INSTANCE] = true
   }
 }
 
 export default ObjArgValidator
+export { IS_OBJ_ARG_VALIDATOR_INSTANCE }
--- tests/unit tests/public/js/validation/ObjArgValidator.test.js
+++ tests/unit tests/public/js/validation/ObjArgValidator.test.js
@@ -1,10 +1,13 @@
 import { vi, describe, it, expect, beforeEach, afterEach } from 'vitest'
-import ObjArgValidator from '../../../../../public/js/validation/ObjArgValidator.js'
+import ObjArgValidator, {
+  IS_OBJ_ARG_VALIDATOR_INSTANCE,
+} from '../../../../../public/js/validation/ObjArgValidator.js'
 import CreateTextOptsSchema from '../../../../../public/js/validation/CreateTextOptsSchema.js'
 import ValidatorConfigSchema from '../../../../../public/js/validation/ValidatorConfigSchema.js'
 import SchemaAdapterBase, {
   IS_SCHEMA_ADAPTER_BASE_INSTANCE,
 } from '../../../../../public/js/abstracts/validation/SchemaAdataperBase.js'
+import Joi from 'joi'
 
 describe('ObjArgValidator', () => {
   class SchemaAdapterMock extends SchemaAdapterBase {
@@ -63,20 +66,126 @@ describe('ObjArgValidator', () => {
     )
   })
 
-  it('when you pass a single valid instance of SingleAdapterBase to constructor, objArgValidator.schema needs to store that argument', () => {
+  it('given a SchemaAdapterMock extends from SchemaAdapterBase, when you pass its instance to constructor, new instance should store it in property whose key is that schemaAdapterMock.paramName and value is schemaAdapterMock.schema, and it should have property that checks instance of constructor', () => {
+    const paramName = 'options'
+    const schemaMock = Joi.object({
+      tagName: Joi.string(),
+    })
+    class SchemaAdapterMock extends SchemaAdapterBase {
+      constructor() {
+        super()
+        this.paramName = paramName
+        this.schema = schemaMock
+      }
+    }
+
+    const schemaAdapterMock = new SchemaAdapterMock()
+
     const result = new ObjArgValidator(schemaAdapterMock)
 
-    expect(result.schema).toEqual(schemaAdapterMock)
+    expect(result[schemaAdapterMock.paramName]).toEqual(
+      schemaAdapterMock.schema
+    )
+    expect(result[IS_OBJ_ARG_VALIDATOR_INSTANCE]).toBe(true)
   })
 
-  it('when you pass an array of instances of SchemaAdaptersBase, objArgValidator.schemas needs to store that array', () => {
-    const schemaAdapter1 = new SchemaAdapterMock()
-    const schemaAdapter2 = new SchemaAdapterMock()
+  it('given 2 different mock classes with different paramName instance property values extended from SchemaAdapterBase, when you pass an array of instances of those 2 mock classes, the new instance should contain 2 properties whose keys are the .propertyName value of the respective mock class, and value is the the respective mock class schema property value, and it should have property that checks instance of constructor', () => {
+    const paramName1 = 'option'
+    const schema1 = Joi.object({
+      tagName: Joi.string(),
+    })
 
-    const schemaAdapters = [schemaAdapter1, schemaAdapter2]
+    class SchemaAdapterMock1 extends SchemaAdapterBase {
+      constructor() {
+        super()
+        this.paramName = paramName1
+        this.schema = schema1
+      }
+    }
+
+    const paramName2 = 'config'
+    const schema2 = Joi.object({
+      tagName: Joi.string(),
+    })
+
+    class SchemaAdapterMock2 extends SchemaAdapterBase {
+      constructor() {
+        super()
+        this.paramName = paramName2
+        this.schema = schema2
+      }
+    }
+
+    const schemaAdapterMock1 = new SchemaAdapterMock1()
+
+    const schemaAdapterMock2 = new SchemaAdapterMock2()
+
+    const schemaAdapters = [schemaAdapterMock1, schemaAdapterMock2]
 
     const result = new ObjArgValidator(schemaAdapters)
 
-    expect(result.schemas).toEqual(schemaAdapters)
+    expect(result[paramName1]).toEqual(schemaAdapterMock1.schema)
+    expect(result[paramName2]).toEqual(schemaAdapterMock2.schema)
+    expect(result[IS_OBJ_ARG_VALIDATOR_INSTANCE]).toBe(true)
+  })
+
+  it('given 3 different mock classes where 2 of them has THE SAME paramName instance property values extended from SchemaAdapterBase, when you pass an array of instances of those 2 mock classes to constructor, it should throw an error', () => {
+    const paramName = 'option'
+    const schema1 = Joi.object({
+      tagName: Joi.string(),
+    })
+
+    class SchemaAdapterMock1 extends SchemaAdapterBase {
+      constructor() {
+        super()
+        this.paramName = paramName
+        this.schema = schema1
+      }
+    }
+
+    const schema2 = Joi.object({
+      tagName: Joi.string(),
+    })
+
+    class SchemaAdapterMock2 extends SchemaAdapterBase {
+      constructor() {
+        super()
+        this.paramName = paramName
+        this.schema = schema2
+      }
+    }
+
+    const paramName3 = 'test'
+    const schema3 = Joi.object({
+      tagName: Joi.string(),
+    })
+
+    class SchemaAdapterMock3 extends SchemaAdapterBase {
+      constructor() {
+        super()
+        this.paramName = paramName3
+        this.schema = schema3
+      }
+    }
+
+    const schemaAdapterMock1 = new SchemaAdapterMock1()
+
+    const schemaAdapterMock2 = new SchemaAdapterMock2()
+
+    const schemaAdapterMock3 = new SchemaAdapterMock3()
+
+    const schemaAdapters = [
+      schemaAdapterMock1,
+      schemaAdapterMock2,
+      schemaAdapterMock3,
+    ]
+
+    const fn = () => {
+      new ObjArgValidator(schemaAdapters)
+    }
+
+    expect(fn).toThrow(
+      'There are SchemaAdapterBase instances with the same .paramName value. Each SchemaAdapterBase instance in the array needs to have unique .paramName value'
+    )
   })
 })
