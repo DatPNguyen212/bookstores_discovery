564b462215a8e0d45a651724cb6fc454e70afc1f
Dat P Nguyen <darkexpert3@gmail.com>
2026-02-11 03:50:49 +0700

    add validationProcessor.checkNoErrors() method

--- public/js/validation/ValidationProcessor.js
+++ public/js/validation/ValidationProcessor.js
@@ -1,34 +1,35 @@
-import { IS_SINGLE_VALIDATOR_BASE_INSTANCE } from "../abstracts/validation/SingleValidatorBase.js";
-import { IS_GROUP_VALIDATOR_BASE_INSTANCE } from "../abstracts/validation/GroupValidatorBase.js";
-import { IS_INPUT_RULES_INSTANCE } from "./InputRules.js";
-import typeCheck from "../utils/typeCheck.js";
-import { IS_INPUT_ERRORS_FACTORY_BASE_INSTANCE } from "../abstracts/validation/InputErrorsFactoryBase.js";
-import ValidationProcessorBase from "../abstracts/validation/ValidationProcessorBase.js";
+import { IS_SINGLE_VALIDATOR_BASE_INSTANCE } from '../abstracts/validation/SingleValidatorBase.js'
+import { IS_GROUP_VALIDATOR_BASE_INSTANCE } from '../abstracts/validation/GroupValidatorBase.js'
+import { IS_INPUT_RULES_INSTANCE } from './InputRules.js'
+import typeCheck from '../utils/typeCheck.js'
+import { IS_INPUT_ERRORS_FACTORY_BASE_INSTANCE } from '../abstracts/validation/InputErrorsFactoryBase.js'
+import ValidationProcessorBase from '../abstracts/validation/ValidationProcessorBase.js'
+import { IS_INPUT_ERRORS_INSTANCE } from './InputErrors.js'
 
 class ValidationProcessor extends ValidationProcessorBase {
   constructor(singleValidator, groupValidator, inputErrorsFactory) {
-    super();
+    super()
     if (!singleValidator?.[IS_SINGLE_VALIDATOR_BASE_INSTANCE]) {
       throw new TypeError(
-        "You need to pass instance of SingleValidatorBase to first parameter",
-      );
+        'You need to pass instance of SingleValidatorBase to first parameter'
+      )
     }
 
     if (!groupValidator?.[IS_GROUP_VALIDATOR_BASE_INSTANCE]) {
       throw new TypeError(
-        "You need to pass instance of GroupValidatorBase in 2nd parameter",
-      );
+        'You need to pass instance of GroupValidatorBase in 2nd parameter'
+      )
     }
 
     if (!inputErrorsFactory?.[IS_INPUT_ERRORS_FACTORY_BASE_INSTANCE]) {
       throw new TypeError(
-        "You need to pass instance of InputErrorsFactoryBase in 3rd parameter",
-      );
+        'You need to pass instance of InputErrorsFactoryBase in 3rd parameter'
+      )
     }
 
-    this.singleValidator = singleValidator;
-    this.groupValidator = groupValidator;
-    this.inputErrorsFactory = inputErrorsFactory;
+    this.singleValidator = singleValidator
+    this.groupValidator = groupValidator
+    this.inputErrorsFactory = inputErrorsFactory
   }
 
   validate(inputRulesArray) {
@@ -36,60 +37,86 @@ class ValidationProcessor extends ValidationProcessorBase {
       for (let inputRules of inputRulesArray) {
         if (!(inputRules && inputRules[IS_INPUT_RULES_INSTANCE])) {
           throw new TypeError(
-            "You need to pass an array of instances of InputRules in 2nd parameter",
-          );
+            'You need to pass an array of instances of InputRules in 2nd parameter'
+          )
         }
       }
     } else {
       throw new TypeError(
-        "You need to pass an array of instances of InputRules in 2nd parameter",
-      );
+        'You need to pass an array of instances of InputRules in 2nd parameter'
+      )
     }
 
-    let inputErrorsArray = [];
+    let inputErrorsArray = []
 
     for (let inputRules of inputRulesArray) {
-      const input = inputRules.input;
-      const rules = Object.keys(inputRules.rules);
+      const input = inputRules.input
+      const rules = Object.keys(inputRules.rules)
 
-      const inputErrors = this.inputErrorsFactory.create(input);
+      const inputErrors = this.inputErrorsFactory.create(input)
 
       if (typeCheck.isSingleInputType(input)) {
         for (let rule of rules) {
-          if (typeof this.singleValidator[rule] !== "function") {
+          if (typeof this.singleValidator[rule] !== 'function') {
             throw new TypeError(
-              `${rule} is not a supported single input validation rule`,
-            );
+              `${rule} is not a supported single input validation rule`
+            )
           } else {
             const errorMsg = this.singleValidator[rule](
               input,
-              inputRules.rules[rule],
-            );
+              inputRules.rules[rule]
+            )
 
-            inputErrors.errors.push(errorMsg);
+            inputErrors.errors.push(errorMsg)
           }
         }
       }
 
-      if (input.type === "checkbox" || input.type === "radio") {
+      if (input.type === 'checkbox' || input.type === 'radio') {
         for (let rule of rules) {
-          if (typeof this.groupValidator[rule] !== "function") {
+          if (typeof this.groupValidator[rule] !== 'function') {
             throw new TypeError(
-              `${rule} rule is not a supported group input validation rule`,
-            );
+              `${rule} rule is not a supported group input validation rule`
+            )
           } else {
-            const errorMsg = this.groupValidator[rule](input);
+            const errorMsg = this.groupValidator[rule](input)
 
-            inputErrors.errors.push(errorMsg);
+            inputErrors.errors.push(errorMsg)
           }
         }
       }
 
-      inputErrorsArray.push(inputErrors);
+      inputErrorsArray.push(inputErrors)
     }
 
-    return inputErrorsArray;
+    return inputErrorsArray
+  }
+
+  checkNoErrors(inputErrorsArray) {
+    if (!Array.isArray(inputErrorsArray)) {
+      throw new TypeError(
+        'You need to pass an array that contains InputErrors instances'
+      )
+    } else {
+      for (let inputErrorsItem of inputErrorsArray) {
+        if (!inputErrorsItem?.[IS_INPUT_ERRORS_INSTANCE]) {
+          throw new TypeError(
+            'You need to pass an array that contains InputErrors instances'
+          )
+        }
+      }
+    }
+
+    const nullOnlyInputErrors = inputErrorsArray.filter((inputErrors) => {
+      return inputErrors.checkAllNull()
+    })
+
+    if (nullOnlyInputErrors.length === inputErrorsArray.length) {
+      return true
+    } else {
+      return false
+    }
   }
 }
 
-export default ValidationProcessor;
+export default ValidationProcessor
